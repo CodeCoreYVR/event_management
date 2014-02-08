811518ad6b22baa1ba34e6a02dda49b7f1865394
drew <123and@gmail.com>
2014-02-07 16:46:59 -0800

    Add Events controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 EventManagement::Application.routes.draw do
+
+
+  root "events#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

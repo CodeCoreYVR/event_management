e6c2efaa9d121aa0603478b58b31b35eb75445cd
donwon <imdonw@gmail.com>
2014-02-10 14:14:22 -0800

    fix alerts and scrolling to form submitting

--- app/assets/javascripts/index_scripts.js
+++ app/assets/javascripts/index_scripts.js
@@ -5,72 +5,72 @@ $(function(){
     }, 800);
     return false;
   });
-// });
-// $(function(){
-//   $("#eventslist2").on('click','input[type=checkbox]',function(e){
-//     var brother="#eventslist input[type='checkbox'][value="+this.value+"]";
-//     if (this.checked){
-//       $(brother).prop('checked', true);
-//     }
-//     else {
-//       $(brother).prop('checked', false);
-//     }
-//   });
-// });
-// $(function(){
-//   $("#eventslist").on('click','input[type=checkbox]',function(e){
-//     var brother="#eventslist2 input[type='checkbox'][value="+this.value+"]";
-//     if (this.checked){
-//       $(brother).prop('checked', true);
-//     }
-//     else {
-//       $(brother).prop('checked', false);
-//     }
-//   });
-// });
-// $(function(){
-//   $("input[name='commit'][type='submit']").on('click',function(e){
-//     var nameMissing=false;
-//     if($('#attendee_name').val()===''){
-//         e.preventDefault();
-//         $('#attendee_name').addClass("inputError");
-//         $('#attendee_name').prop('placeholder',"Input required");
-//         $('#attendee_name').focus();
-//         nameMissing=true;
-//     }
-//     if($('#attendee_email').val()===''){
-//       $('#attendee_email').addClass("inputError");
-//       $('#attendee_email').prop('placeholder',"Input required");
-//       if (!nameMissing){
-//         e.preventDefault();
-//         $('#attendee_email').focus();
-//       }
-//     }
-//     else{
-//       var email_pat = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,6}$/i;
-//       if (email_pat.exec($('#attendee_email').val())===null){
-//         $('#attendee_email').addClass("inputError");
-//         $('#attendee_email').val(null);
-//         $('#attendee_email').prop('placeholder',"Email is wrong");
-//         if (!nameMissing){
-//           e.preventDefault();
-//           $('#attendee_email').focus();
-//         }
-//       }
-//     }
-//   });
-// });
-// $(function(){
-//   $('#attendee_name').on('keyup',function(e){
-//     if($('#attendee_name').val()!=''){
-//         $('#attendee_name').removeClass("inputError");
-//     }
-//   });
-// });
-// $(function(){
-//   $('#attendee_email').on('keyup',function(e){
-//     if($('#attendee_email').val()!=''){
-//         $('#attendee_email').removeClass("inputError");
-//     }
-//   });
-// });
+});
+$(function(){
+  $("#eventslist2").on('click','input[type=checkbox]',function(e){
+    var brother="#eventslist input[type='checkbox'][value="+this.value+"]";
+    if (this.checked){
+      $(brother).prop('checked', true);
+    }
+    else {
+      $(brother).prop('checked', false);
+    }
+  });
+});
+$(function(){
+  $("#eventslist").on('click','input[type=checkbox]',function(e){
+    var brother="#eventslist2 input[type='checkbox'][value="+this.value+"]";
+    if (this.checked){
+      $(brother).prop('checked', true);
+    }
+    else {
+      $(brother).prop('checked', false);
+    }
+  });
+});
+$(function(){
+  $("input[name='commit'][type='submit']").on('click',function(e){
+    var nameMissing=false;
+    if($('#attendee_name').val()===''){
+        e.preventDefault();
+        $('#attendee_name').addClass("inputError");
+        $('#attendee_name').prop('placeholder',"Input required");
+        $('#attendee_name').focus();
+        nameMissing=true;
+    }
+    if($('#attendee_email').val()===''){
+      $('#attendee_email').addClass("inputError");
+      $('#attendee_email').prop('placeholder',"Input required");
+      if (!nameMissing){
+        e.preventDefault();
+        $('#attendee_email').focus();
+      }
+    }
+    else{
+      var email_pat = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,6}$/i;
+      if (email_pat.exec($('#attendee_email').val())===null){
+        $('#attendee_email').addClass("inputError");
+        $('#attendee_email').val(null);
+        $('#attendee_email').prop('placeholder',"Email is wrong");
+        if (!nameMissing){
+          e.preventDefault();
+          $('#attendee_email').focus();
+        }
+      }
+    }
+  });
+});
+$(function(){
+  $('#attendee_name').on('keyup',function(e){
+    if($('#attendee_name').val()!=''){
+        $('#attendee_name').removeClass("inputError");
+    }
+  });
+});
+$(function(){
+  $('#attendee_email').on('keyup',function(e){
+    if($('#attendee_email').val()!=''){
+        $('#attendee_email').removeClass("inputError");
+    }
+  });
+});

881483144196f19c063ec4041deb7aaac4394a30
Arash <arash.ouji@safe.com>
2014-02-10 17:01:56 -0800

    Add seat validation to event

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,7 @@ class Event < ActiveRecord::Base
   has_many :attendances, dependent: :destroy
   has_many :attendees, through: :attendances
   has_attached_file :image, :styles => { :medium => "100x100>", :thumb => "100x100>" }, :default_url => 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTdsjWnPtvccjwXi18Hbab91KDKChPoWSMCF0maMUBMjSuwAKQL'
-
+  validates_numericality_of :seats
   validates_attachment_size :image, less_than: 10.megabytes
 
   validate :date_checker

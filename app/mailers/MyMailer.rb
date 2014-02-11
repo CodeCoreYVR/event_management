6248e771f5e9ec487acb5f6a193530b672659a05
Jef Curtis <jefcurtis@gmail.com>
2014-02-10 17:43:24 -0800

    remove MyMailer

--- app/mailers/MyMailer.rb
+++
@@ -1,9 +0,0 @@
-class MyMailer < ActionMailer::Base
-	def confirmation_email(user)
-		@user = user
-
-		mail(to: user.email,
-			   From: 'brandontricci@gmail.com' 
-			   Subject: 'Confirmation Email From CodeCore Events')
-	end
-end

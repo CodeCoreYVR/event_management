fe5c7e80ade990c6a049e5f2f3063b2e3052097b
donwon <imdonw@gmail.com>
2014-02-10 17:00:39 -0800

    changed error message when class is full

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -7,7 +7,8 @@ class Attendance < ActiveRecord::Base
     event=Event.find("#{event_id}")
     event_seats_taken=Attendance.where("event_id=#{event_id}").length
     if (event.seats <= event_seats_taken)
-      errors.add(:event_id," #{event_id} has been filled. Sorry!" )
+
+      errors.add(:event_id," #{event.title} has been filled. Sorry!" )
     end
   end
 

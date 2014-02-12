d14a42b6aa11660419f18fcb35694eae28f9f9d0
Arash <arash.ouji@safe.com>
2014-02-11 18:03:32 -0800

    Add image upload support, fix date and time diplay and misc css changes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
 
   def index
-    @events = Event.all
+    @events = Event.all.order("date ASC")
     @attendee = Attendee.new
   end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,9 @@
 module ApplicationHelper
 
   def formatted_date(date)
-    date.strftime("%d-%B-%Y")
+    date.strftime("%a, %b %m, %Y")
+  end
+  def formatted_time(date)
+    date.strftime("%I:%M %p")
   end
 end
